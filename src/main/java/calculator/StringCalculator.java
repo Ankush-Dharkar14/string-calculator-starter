4cdac4e7c8f0dc713544f87f6a1b9b1e4c8f8360
Ankush-Dharkar14 <ankushdharkar01451@gmail.com>
2021-10-17 14:37:10 +0530

    Committed first point

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,8 +2,29 @@ package calculator;
 
 class StringCalculator {
 
-    public int add(String input) {
-        return 0;
-    }
+	public int add(String numbers) {
+		String[] inputNumbers = numbers.split(",");
+		if (isEmpty(numbers)) {
+			return 0;
+		}
+		if (numbers.length() == 1) {
+			return convertStringToInt(numbers);
+		}
+		return sumValues(inputNumbers[0],inputNumbers[1]);
+	}
+
+	private boolean isEmpty(String num) {
+		return num.isEmpty();
+	}
+
+	private int convertStringToInt(String num) {
+		return Integer.parseInt(num);
+	}
+
+	private int sumValues(String num1,String num2) {
+		return convertStringToInt(num1)+convertStringToInt(num2);
+		 
+
+	}
 
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -1,20 +1,31 @@
 package calculator;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 
 class StringCalculatorShould {
 
-    @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
-    }
+	private StringCalculator sc;
 
-    @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
-    }
+	@BeforeEach
+	void initialize() {
+		sc = new StringCalculator();
+	}
+
+	@Test
+	public void test_StringIsEmpty_Return0() {
+		assertEquals(sc.add(""), 0);
+	}
+
+	@Test
+	public void test_StringHas1Value_ReturnOnly1_Value() {
+		assertEquals(sc.add("1"), 1);
+	}
+
+	@Test
+	public void test_StringHasTwoValues_ReturnSum() {
+		assertEquals(sc.add("1,2"), 3);
+	}
+
 }
